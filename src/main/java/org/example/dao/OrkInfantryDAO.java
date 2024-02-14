090ef4dc12fa45fffb147cd381503431effc1c81
Ottowski <otto.arvidsson94@gmail.com>
2024-02-14 16:40:30 +0100

    OrkInfantryDAO

--- src/main/java/org/example/dao/OrkInfantryDAO.java
+++ src/main/java/org/example/dao/OrkInfantryDAO.java
@@ -1,12 +1,12 @@
 package org.example.dao;
-import org.example.entity.OrkInfantryLeaderEntity;
+import org.example.entity.OrkInfantryEntity;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class OrkInfantryLeaderDAO {
+public class OrkInfantryDAO {
     // JDBC URL, username, and password of MySQL server
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/New Warhammer Database";
     private static final String USER = "root";
@@ -18,22 +18,24 @@ public class OrkInfantryLeaderDAO {
     private static ResultSet resultSet;
 
     // SQL queries
-    private static final String INSERT_QUERY = "INSERT INTO orkinfantryleader (name, movement, toughness, " +
-            "wounds, save, meleeWeapon, rangedWeapon, pistol, specialUnitRangedWeapon) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    private static final String INSERT_QUERY = "INSERT INTO orkinfantry (unitName, unitLeader, unitSize, movement, toughness, " +
+            "wounds, save, meleeWeapon, rangedWeapon, pistol, specialUnitRangedWeapon) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-    public void insertOrkInfantryLeader(OrkInfantryLeaderEntity orkInfantryLeader) {
+    public void insertOrkInfantry(OrkInfantryEntity orkInfantry) {
         try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
             preparedStatement = connection.prepareStatement(INSERT_QUERY);
-            preparedStatement.setString(1, orkInfantryLeader.getName());
-            preparedStatement.setInt(2, orkInfantryLeader.getMovement());
-            preparedStatement.setInt(3, orkInfantryLeader.getToughness());
-            preparedStatement.setInt(4, orkInfantryLeader.getWounds());
-            preparedStatement.setInt(5, orkInfantryLeader.getSave());
-            preparedStatement.setInt(6, orkInfantryLeader.getMeleeWeapon());
-            preparedStatement.setInt(7, orkInfantryLeader.getRangedWeapon());
-            preparedStatement.setInt(8, orkInfantryLeader.getPistol());
-            preparedStatement.setInt(8, orkInfantryLeader.getSpecialUnitRangedWeapon());
+            preparedStatement.setString(1, orkInfantry.getUnitName());
+            preparedStatement.setInt(2, orkInfantry.getUnitLeader());
+            preparedStatement.setInt(2, orkInfantry.getUnitSize());
+            preparedStatement.setInt(2, orkInfantry.getMovement());
+            preparedStatement.setInt(3, orkInfantry.getToughness());
+            preparedStatement.setInt(4, orkInfantry.getWounds());
+            preparedStatement.setInt(5, orkInfantry.getSave());
+            preparedStatement.setInt(6, orkInfantry.getMeleeWeapon());
+            preparedStatement.setInt(7, orkInfantry.getRangedWeapon());
+            preparedStatement.setInt(8, orkInfantry.getPistol());
+            preparedStatement.setInt(8, orkInfantry.getSpecialUnitRangedWeapon());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
